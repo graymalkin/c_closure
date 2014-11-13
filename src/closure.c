a45617b4bd6a2ee37cc9500996b4806e1a3656c7
Simon Cooksey <sjc80@kent.ac.uk>
2014-11-13 15:12:28 +0000

    Used GCC inline function to realise a closure.

--- src/closure.c
+++ src/closure.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "closure.h"
 
-
 int main(int argc, char* argvp[])
 {
+	test_one();
+	test_two();
 
 	return 0;
 }
 
+void test_one()
+{
+	/* Out of line definition of the function */
+	closure k;
+	k.x = 5;
+	k.y = 7;
+	int value = 11;
+	k.f = sum;
+
+	int result = k.f(k.x, k.y, value);
+	printf("Result: %d\n", result);
+	assert(result == 23);
+}
+
+// ISO C forbids nested functions. I disagree.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+void test_two()
+{
+	closure k;
+	k.x = 2;
+	k.y = 6;
+	int value = 14;
+	int (*fun) (int a, int b, int c) = ({
+		int sum_inline(int a, int b, int c) 
+		{ 
+			return a + b + c;
+		} sum_inline; 
+	});
+	k.f = fun;
+
+	int result = k.f(k.x, k.y, value);
+	printf("Result: %d\n", result);
+	assert(result == 22);
+}
+#pragma GCC diagnostic pop
+
+int sum(int a, int b, int c) {
+	return a + b + c;
+}
 
--- src/closure.h
+++ src/closure.h
@@ -2,12 +2,13 @@
 #define __closure_h_
 
 typedef struct closure {
-	int (*f)(int a, int b, int z);
-	int x;
-	int y;
+	int (*f)(int a, int b, int c);
+	int x, y;
 } closure;
 
 int main(int argc, char* argvp[]);
-
+void test_one();
+void test_two();
+int sum(int a, int b, int c);
 
 #endif 
